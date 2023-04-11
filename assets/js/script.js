dc1f01c4656a5a71766ab519ca0bd66ceb7495d7
Sirinya Richardson <siri.richardson@gmail.com>
2023-04-11 14:45:43 +0200

    Add proces options function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultModal = new bootstrap.Modal(document.getElementById('resultsModal'));
 
 document.getElementById('status').addEventListener('click', e => getStatus(e));
+document.getElementById('submit').addEventListener('click', e => postForm(e));
 
 async function getStatus(e) {
   const queryString = `${API_URL}?api_key=${API_KEY}`;
@@ -21,5 +22,73 @@ function displayStatus(data) {
   const resultsModalContent = document.getElementById('results-content');
   resultsModalTitle.innerText = "API Key Status";
   resultsModalContent.innerHTML = `<div>Your key is valid until</div><div class="key-status">${data.expiry}</div>`;
+  resultModal.show();
+}
+
+// function processFormToJson(form) {
+//   var formObject = {};
+//   form.forEach(createFormObject);
+//   return JSON.stringify(formObject);
+//
+//   function createFormObject(value, key) {
+//     formObject[key] = value;
+//   }
+// }
+
+function processOptions(form) {
+  let optArray = [];
+
+  for (let e of form.entries()) {
+    if (e[0] === "options") {
+      optArray.push(e[1]);
+    }
+  }
+
+  form.delete("options");
+
+  form.append("options", optArray.join());
+
+  return form;
+}
+
+async function postForm(e) {
+  var formElement = document.getElementById('checksform');
+  var form = new FormData(formElement);
+  form = processOptions(form);
+
+  const queryOption = {
+    method: "POST",
+    headers: {
+      "Authorization": API_KEY
+    },
+    body: form
+  };
+
+  const response = await fetch(API_URL, queryOption);
+  const data = await response.json();
+
+  if (response.ok) {
+    displayErrors(data);
+  }  else {
+    throw new Error(data.error);
+  }
+}
+
+function displayErrors(data) {
+  let results = "";
+  let heading = `JSHint Results for ${data.file}`;
+  if (data.total_errors === 0) {
+    results = `<div class="no_errors">No errors reported!</div>`;
+  } else {
+    results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
+    for (let error of data.error_list) {
+      results += `<div>At line <span class="line">${error.line}</span>, `;
+      results += `column <span class="column">${error.col}:</span></div>`;
+      results += `<div class="error">${error.error}</div>`;
+    }
+  }
+
+  document.getElementById("resultsModalTitle").innerText = heading;
+  document.getElementById("results-content").innerHTML = results;
   resultModal.show();
 }
